6f190d2ebed169a5caef704bc70e430a40ea7ba3
ekkuleivonen <ekku.leivonen@gmail.com>
2022-07-09 00:59:52 +0100

    notification

--- src/components/Tool-bar-menus/Right-menu.tsx
+++ src/components/Tool-bar-menus/Right-menu.tsx
@@ -1,6 +1,8 @@
 import { Component, createSignal } from "solid-js";
 import styles from "./menus.module.css";
 import wifiIcon from "../../assets/wifi-icon.webp";
+import lowBatteryIcon from "../../assets/low-battery.svg";
+import Notification from "../Alert-popup/Notification";
 
 const RightMenu: Component = () => {
   const [dateTime, setDateTime] = createSignal<string | null>(null);
@@ -24,6 +26,7 @@ const RightMenu: Component = () => {
 
   return (
     <div class={styles.rightMenu}>
+      <Battery />
       <img
         src={wifiIcon}
         alt="wifi icon"
@@ -35,3 +38,37 @@ const RightMenu: Component = () => {
 };
 
 export default RightMenu;
+
+function Battery() {
+  const [batteryLevel, setBatteryLevel] = createSignal<number>(77);
+
+  const dechargeBattery = () => {
+    if (batteryLevel() < 1) return; // don't decharge if battery is already empty
+    //TODO: maybe trigger a warning if battery is low
+    setBatteryLevel(batteryLevel() - 1);
+    const batteryLevelDiv = document.getElementById("batteryLevel");
+    if (batteryLevelDiv) batteryLevelDiv.style.width = `${batteryLevel()}%`;
+  };
+
+  setInterval(dechargeBattery, 1000 * 10);
+
+  const notification = {
+    img: lowBatteryIcon,
+    title: "Battery low",
+    message: "Your battery is low. Please charge it.",
+    btnText: "Charge",
+  };
+
+  return (
+    <>
+      <div class={styles.menuItem}>
+        <p>{`${batteryLevel()} %`}</p>
+        <div class={styles.batteryShell}>
+          <div class={styles.batteryLevel} id="batteryLevel" />
+          <div class={styles.batteryKnob} />
+        </div>
+      </div>
+      <Notification notification={notification} />
+    </>
+  );
+}
